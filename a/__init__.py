f5f49ff210909eb22ea5cef74f8b172dece0594b
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-09-06 18:07:29 +0800

    more

--- a/__init__.py
+++ a/__init__.py
@@ -1,9 +1,9 @@
 import os
 import sys
-from a.b import SomeClass as SomeClassA
 
 sys.path.append(os.path.dirname(__file__))
 
+from a.b import SomeClass as SomeClassA
 from b import SomeClass as SomeClassB
 
 
