1f64ec0f7008676e7d9428b9280b4f05aaeb1a61
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-09-06 17:02:50 +0800

    more

--- a/b/c/__init__.py
+++ a/b/c/__init__.py
@@ -1,5 +1,10 @@
+import os
+import sys
 from a.b.c.d import SomeClass as SomeClassA
-from b.c.d import SomeClass as SomeClassB
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from c.d import SomeClass as SomeClassB
 
 
 def SomeFunc():
